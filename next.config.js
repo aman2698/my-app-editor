92d17b8e8eb2199d5a4c617f47bf9479c1ccbb6e
Aman <sikarwaraman26@gmail.com>
2023-11-19 12:16:07 +0530

    fdf

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-
-    // Add basePath
-     basePath: '/github-pages',
+    trailingSlash: true,
+    output: 'export',
    
 }
 
